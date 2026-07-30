04afd5ed72e4811a8ba9fbee6e7ab0669d8e93de
edwinmovexa-star <edwinmovexa@gmail.com>
2026-07-30 16:25:49 -0600

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// Reemplaza estos valores con la configuración de tu aplicación web de Firebase.
+// Firebase Console > Configuración del proyecto > Tus apps > SDK setup and configuration.
+export  const firebaseConfig = {
+    apiKey: "AIzaSyDTN5peAbbRY_T-54nPU6V3Gz7lJJdKDYw",
+    authDomain: "noventia-inventario.firebaseapp.com",
+    projectId: "noventia-inventario",
+    storageBucket: "noventia-inventario.firebasestorage.app",
+    messagingSenderId: "384038093167",
+    appId: "1:384038093167:web:f4b9aea4a2e6280d79a89a"
+  };
+
